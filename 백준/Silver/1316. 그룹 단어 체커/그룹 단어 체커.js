85ea74b6264d84c5ce6de8972b34223585e5cec2
Han HyeYoung <88419759+HanHoing@users.noreply.github.com>
2022-12-14 00:15:50 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,21 +1,17 @@
-const inputData = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+var inputData = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+var inSize = Number(inputData.shift());
+var yGroup = inSize;
 
-let inSize = Number(inputData.shift());
-let yGroup = inSize;
-let a;
-
-for(let i=0; i <inSize; i++){         
-    a = inputData[i].replace('\r','').split(''); //개행제거
+for(var i=0; i <inSize; i++){         
+    var a = inputData[i].replace('\r','').split(''); 
 
     if(a.length > 2){ 
-        for(let j=1; j < a.length; j++){
-            let b = a.indexOf(a[j]);   
-            if((j-b > 1) && (a[j]!=a[j-1])){ 
+        for(var j=1; j < a.length; j++){
+            if((j- a.indexOf(a[j]) > 1) && (a[j]!=a[j-1])){ 
                 yGroup--;
                 break;
             }
         }
     }
 }
-
 console.log(yGroup);
