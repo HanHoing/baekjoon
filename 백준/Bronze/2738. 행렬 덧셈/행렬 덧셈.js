a1fa3e2afcc06b3623aba2a947e69aec3baa5e23
Han HyeYoung <88419759+HanHoing@users.noreply.github.com>
2023-01-21 13:20:42 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 128 ms, Memory: 11152 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
@@ -1,22 +1,20 @@
 var filepath = (process.platform == 'win32')?__dirname+'/test.txt':'/dev/stdin';
 var inputData = require('fs').readFileSync(filepath).toString().split('\n');
-var NM = inputData.shift().split(' '); //NM[0]=행, NM[1]=열
-var N = parseInt(NM.shift());
-var M = parseInt(NM.shift());
 
-
+var [N, M] = inputData.shift().split(' ').map(e=>+e); //N=행, M=열
 
 var A = new Array();
 var B = new Array();
 var sum = '';
 
 for(var i=0; i<N; i++){
-        A[i] = inputData[i].split(' ').map(e=>+e); 
-        B[i] = inputData[i+N].split(' ').map(e=>+e); 
-        for(var j=0; j<M; j++){
-            sum += parseInt(A[i][j]+B[i][j]) +' ';
-        }
-       if(i!=N-1) sum+='\n'
+    A.push(inputData[i].split(' ').map(e=>+e)); 
+    B.push(inputData[i+N].split(' ').map(e=>+e));
+    for(var j=0; j<M; j++){
+        sum += A[i][j]+B[i][j] +' ';
+    }
+    if(i!=N-1) sum+='\n';
 }
 
 console.log(sum);
+
