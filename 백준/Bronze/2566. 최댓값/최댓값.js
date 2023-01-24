0c74bdfbe1f4021a2f3b677bc7938feb7da502a0
Han HyeYoung <88419759+HanHoing@users.noreply.github.com>
2023-01-24 13:58:49 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.js
+++ 백준/Bronze/2566. 최댓값/최댓값.js
@@ -1,22 +1,11 @@
-var filepath = (process.platform == 'win32')?__dirname+'/test.txt':'/dev/stdin';
-var data = require('fs').readFileSync(filepath).toString().split('\n');
-
-var max = 0; 
-var row=1, col=1; 
+var data = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 var arr = new Array();
 for(var i=0; i<data.length; i++)
-    arr.push(data[i].split(' ').map(Number));
+    arr.push(...data[i].split(' ').map(e=>+e));  //... : 배열에서 꺼내서 넣기 
 
-arr.forEach((val, i)=>{
-    arr[i].forEach((val, j) => {
-        if(val > max){
-            max = val; 
-            row = i+1;
-            col = j+1;
-        }
-    });
-});
-
+var max = Math.max(...arr); 
 console.log(max);
-console.log(row+' '+col);
+
+var index = arr.indexOf(max);  
+console.log(Math.floor(index/9+1) + " " + (index%9+1)); //행, 열
